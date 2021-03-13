8df2df201656ef9e1c0a79a985ea78a08bc401b3
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-13 23:27:59 +0900

    攻撃の実装

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//バトル管理
+//PlayerとEnamyを取得
+//攻撃する
+
+public class BattleManager : MonoBehaviour
+{
+    //player取得
+    public Unitmanager player;
+    //enamy取得
+    public Unitmanager enamy;
+
+    void Start()
+    {
+        player.Attack(enamy);
+        enamy.Attack(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
---
+++ Assets/Scripts/Unitmanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//キャラ管理
+//ステータス、攻撃関数、ダメージ関数
+
+public class Unitmanager : MonoBehaviour
+{
+    //ステータス
+    public int hp;
+    public int at;
+
+
+    //攻撃関数
+    public void Attack(Unitmanager target)
+    {
+        target.Damage(at);
+    }
+
+
+    //ダメージ関数
+    void Damage(int damage)
+    {
+        hp -= damage;
+        Debug.Log(name+"は"+damage+"ダメージ");
+    }
+}
